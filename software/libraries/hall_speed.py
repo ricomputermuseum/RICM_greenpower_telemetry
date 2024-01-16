3afc09aba1ffbf3af141b167f31cdbd4274d305c
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2024-01-16 18:41:52 -0500

    updated hall library
    Moved hall library to an interrupt-updated interval calculation

--- software/libraries/hall_speed.py
+++ software/libraries/hall_speed.py
@@ -2,32 +2,35 @@ from micropython import const
 import machine
 import time
 
+last_tick = time.ticks_us()
+last_interval = 1;
+
+def hall_handler(pin): #handler to update speed sensor interval when hall sensor is tripped
+    t = time.ticks_us()
+    global last_interval = time.ticks_diff(t,last_tick)
+    global last_tick = t
+    
+
 #a class abstraction for a digital hall effect sensor used as a rev counter
 
 class hall():
     
-    def __init__(self, pin, diameter, scale):
-        self.pin = machine.Pin(pin)
-        self.scale = scale*diameter #the scale factor of how many revolutions of the magnet per revolution of the tire
-        self.ticks = time.ticks_us();
-        self.period_us = 0
-        
-    def update_period(self):
-        t = time.ticks_us()
-        period_us = time.ticks_diff(self.ticks, t)
-        self.ticks = t()
+    def __init__(self, pin, diameter):
+        self.pin = machine.Pin(pin, machine.Pin.IN)
+        self.scale = 5280/(6.28*(diameter/12)) #number of revolutions per mile given wheel diameter in inches
+        self.pin.irq(trigger=Pin.IRQ_RISING, handler=hall_handler)
         
     def get_rpm(self):
-        return(1000000/self.period_us);
+        return((1000000/last_interval)*60)
     
     def get_speed(self):
-        return(self.get_rpm()*scale)
+        return((scale/self.get_rpm())*60)
     
     def get_speed_kph(self):
         return(self.get_speed()*1.6)
     
 def hall_1_debug():
-    hall1 = machine.Pin(19, machine.Pin.IN)
+    hall1 = hall(19,12)
     for i in range(1000):
-        print(hall1.value())
-        time.sleep_ms(100)    
+        print(hall1.get_speed)
+        time.sleep_ms(1000)    
